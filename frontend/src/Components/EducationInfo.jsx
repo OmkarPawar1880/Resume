77e20ad2dc56c810715781a45ef56ae0a13af855
Omkarpawar1880 <omkarpawar1880@gmail.com>
2026-01-09 21:09:30 +0530

    added backend

--- frontend/src/Components/EducationInfo.jsx
+++ frontend/src/Components/EducationInfo.jsx
@@ -14,6 +14,7 @@ const createEmptyEducation = () => ({
 });
 
 const EducationInfo = () => {
+
   
   const [errors, setErrors] = useState([]);
   const { resume, replaceSection , savePartialResumeToBackend} = useResume();
